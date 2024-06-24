83cd59a47496a318bf22c04a691213eba9078a8e
moon088 <147430525+moon088@users.noreply.github.com>
2024-06-25 00:06:11 +0900

    Add files via upload

---
+++ generate_templete.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+
+default_output_directory = 'AtCoder'
+num_files_to_generate = 4  # 生成するファイルの個数（デフォルトは4個）
+output_directory = default_output_directory
+
+if len(sys.argv) >= 2:
+    num_files_to_generate = int(sys.argv[1])
+
+if len(sys.argv) == 3:
+    output_directory = sys.argv[2]
+
+# テンプレートファイルのパス
+template_path = 'AtCoder\Template.py'
+
+# 生成するファイル名と対応するコメントのリスト
+files = ['A.py', 'B.py', 'C.py', 'D.py', 'E.py', 'F.py']
+comments = ['# A', '# B', '# C', '# D', '# E', '# F']
+
+#files =[".py", ".py", ".py", ".py"]
+
+with open(template_path, 'r') as template_file:
+    template_content = template_file.read()
+
+
+
+for filename, comment in zip(files[:num_files_to_generate], comments[:num_files_to_generate]):
+    output_path = os.path.join(output_directory, filename)
+    with open(output_path, 'w') as new_file:
+        new_file.write(f"{comment}\n{template_content}")
