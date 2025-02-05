cc9be3f22b724f1c660e75cfccaacf88d97fab63
moon088 <147430525+moon088@users.noreply.github.com>
2025-02-06 07:13:24 +0900

    Add files via upload

---
+++ Contest/Biginer/Road Reduction.py
@@ -0,0 +1,62 @@
+# E
+import sys, re
+from collections import deque, defaultdict, Counter
+from math import ceil, floor, sqrt, hypot, factorial, pi, sin, cos, tan, asin, acos, atan, atan2, radians, degrees, log2, gcd
+from cmath import phase
+from itertools import accumulate, chain, combinations, combinations_with_replacement, permutations, compress, dropwhile, takewhile, groupby, product, starmap
+from functools import cmp_to_key,lru_cache
+from operator import itemgetter, mul, xor
+from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase, digits
+from bisect import bisect, bisect_left, insort, insort_left
+from heapq import heappush , heappop
+from functools import reduce, lru_cache
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from decimal import Decimal, ROUND_HALF_UP
+#it is code to make recursion a bit faster
+#Pimport pypyjit
+#pypyjit.set_param("max_unroll_recursion=-1")
+#recursion limit
+sys.setrecursionlimit(10**7)
+def input(): return sys.stdin.readline().strip()
+def list_input(): return list(input())
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def LIST(): return list(map(int,  input().split()))
+def LIST_3D(n): return [[LIST() for _ in range(n)] for _ in range(n)] #Axyz is (y*z)*x
+def MIXED_LIST(): return [int(x) if x.isdigit() else x for x in input().split()]
+def TUPLE(): return tuple(map(int, input().split()))
+def ZIP(n): return map(list, zip(*(map(int, input().split()) for _ in range(n))))
+def BOARD(H): return [list(input().strip()) for _ in range(H)]
+def BOARD_INT(H): return [list(map(int, input().split())) for _ in range(H)]
+def isInBoard(H,W,y,x): return 0<=y<H and 0<=x<W
+def ROUND_OFF(X,k): return  (X // (10 ** k) + 5) // 10 * 10 ** (k + 1)
+def YES(): return print("Yes")
+def NO(): return print("No")
+Dxy = [(1,0),(-1,0),(0,1),(0,-1)]
+INF = 1 << 60
+
+N,M = MAP()
+edge = [[] for v in range(N)]
+for i in range(M):
+    u,v,c =MAP()
+    edge[u-1].append((v-1,c,i))
+    edge[v-1].append((u-1,c,i))
+
+dist = [10**17] * N
+dist[0] = 0
+pq = [(0,0)]
+pre_e = [-1] * N
+while pq:
+    d,v = heappop(pq)
+    if dist[v] < d:
+        continue
+    for nv,c,i in edge[v]:
+        if dist[v] + c < dist[nv]:
+            dist[nv] = dist[v] + c
+            pre_e[nv] = i
+            heappush(pq,(dist[nv],nv))
+
+res = [pre_e[i]+1 for i in range(1,N)]
+res.sort()
+print(*res)
---
+++ Contest/Biginer/XX to XXX.py
@@ -0,0 +1,77 @@
+# C
+import sys, re
+from collections import deque, defaultdict, Counter
+from math import ceil, floor, sqrt, hypot, factorial, pi, sin, cos, tan, asin, acos, atan, atan2, radians, degrees, log2, gcd
+from cmath import phase
+from itertools import accumulate, chain, combinations, combinations_with_replacement, permutations, compress, dropwhile, takewhile, groupby, product, starmap
+from functools import cmp_to_key,lru_cache
+from operator import itemgetter, mul, xor
+from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase, digits
+from bisect import bisect, bisect_left, insort, insort_left
+from heapq import heappush , heappop
+from functools import reduce, lru_cache
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from decimal import Decimal, ROUND_HALF_UP
+#it is code to make recursion a bit faster
+#Pimport pypyjit
+#pypyjit.set_param("max_unroll_recursion=-1")
+#recursion limit
+sys.setrecursionlimit(10**7)
+def input(): return sys.stdin.readline().strip()
+def list_input(): return list(input())
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def LIST(): return list(map(int,  input().split()))
+def LIST_3D(n): return [[LIST() for _ in range(n)] for _ in range(n)] #Axyz is (y*z)*x
+def MIXED_LIST(): return [int(x) if x.isdigit() else x for x in input().split()]
+def TUPLE(): return tuple(map(int, input().split()))
+def ZIP(n): return map(list, zip(*(map(int, input().split()) for _ in range(n))))
+def BOARD(H): return [list(input().strip()) for _ in range(H)]
+def BOARD_INT(H): return [list(map(int, input().split())) for _ in range(H)]
+def isInBoard(H,W,y,x): return 0<=y<H and 0<=x<W
+def ROUND_OFF(X,k): return  (X // (10 ** k) + 5) // 10 * 10 ** (k + 1)
+def YES(): return print("Yes")
+def NO(): return print("No")
+Dxy = [(1,0),(-1,0),(0,1),(0,-1)]
+INF = 1 << 60
+
+
+S=input()
+T=input()
+
+if len(S)>=len(T):
+    YES() if S==T else NO()
+    exit()
+    
+def run_length_encode(s):
+    encoded = []
+    count = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            count += 1
+        else:
+            encoded.append((s[i - 1], count)) 
+            count = 1
+    encoded.append((s[-1], count)) 
+    return encoded  
+
+s,t = run_length_encode(S), run_length_encode(T)
+
+
+if len(s)!=len(t):
+    NO()
+    exit()
+    
+for i in range(len(s)):
+
+    if s[i][0]!=t[i][0]:
+        NO()
+        exit()
+    if s[i][1]==t[i][1] or ((s[i][1]<t[i][1]) and s[i][1]>=2):
+        continue
+    else:
+        NO()
+        exit()
+        
+YES()
